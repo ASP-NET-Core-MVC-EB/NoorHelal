895b98bfbda052c87ea2e0663e27ed5ecaa74709
noorAbd230 <znoorabd050@gmail.com>
2026-08-03 08:43:02 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ResturantApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
